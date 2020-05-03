7d6cf082354789530b96dcc13a715102217bc441
sunny <mr.sunny.narula@gmail.com>
2020-05-03 17:15:56 +0530

    Added starting code for lesson 3

---
+++ Lesson03-Miwok/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,35 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(@NonNull Context context, ArrayList<Word> listOfWords) {
+        super(context, 0, listOfWords);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView == null) {
+            Context context = getContext();
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            convertView = layoutInflater.inflate(R.layout.list_item, parent, false);
+        }
+        TextView mewokWordTextView = convertView.findViewById(R.id.mewok_word);
+        TextView englishWordTextView = convertView.findViewById(R.id.english_word);
+        Word word = getItem(position);
+        mewokWordTextView.setText(word.getMewokTranslation());
+        englishWordTextView.setText(word.getDefaultTranslation());
+        return convertView;
+    }
+}
