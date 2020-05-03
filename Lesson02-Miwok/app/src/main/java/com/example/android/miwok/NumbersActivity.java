32fd5e149f23a54a4d34f3e312c83563d7fdf5e2
sunny <mr.sunny.narula@gmail.com>
2020-05-03 08:51:17 +0530

    2.1 Create an array of for storing the numbers

--- Lesson02-Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Lesson02-Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -9,5 +9,16 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        String[] words = new String[10];
+        words[0] = "One";
+        words[1] = "Two";
+        words[2] = "Three";
+        words[3] = "Four";
+        words[4] = "Five";
+        words[5] = "Six";
+        words[6] = "Seven";
+        words[7] = "Eight";
+        words[8] = "Nine";
+        words[9] = "Ten";
     }
 }
