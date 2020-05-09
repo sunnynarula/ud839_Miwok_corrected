fe663894b2f74dfaec4e222db1f0ca7385869d73
sunny <mr.sunny.narula@gmail.com>
2020-05-09 09:44:43 +0530

    6.5 Add tabs and styles to match design specifications

--- Lesson05-Miwok/app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ Lesson05-Miwok/app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -10,14 +11,17 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 public class CategoryAdapter extends FragmentPagerAdapter {
 
+    private final Context context;
+
     /**
      * Create a new {@link CategoryAdapter} object.
      *
      * @param fm is the fragment manager that will keep each fragment's state in the adapter
      *           across swipes.
      */
-    public CategoryAdapter(FragmentManager fm) {
+    public CategoryAdapter(FragmentManager fm, Context context) {
         super(fm);
+        this.context = context;
     }
 
     /**
@@ -34,6 +38,20 @@ public class CategoryAdapter extends FragmentPagerAdapter {
                 return new ColorsFragment();
             default:
                 return new PhrasesFragment();
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return context.getString(R.string.category_numbers);
+            case 1:
+                return context.getString(R.string.category_family);
+            case 2:
+                return context.getString(R.string.category_colors);
+            default:
+                return context.getString(R.string.category_phrases);
         }
     }
 
